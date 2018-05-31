f620f516890f440bd528f652b168f10989735f2c
John Kloor <john@kloor.org>
2018-05-31 08:29:36 -0400

    Ensure browsers are on HTTPS.

--- app/middleware.php
+++ app/middleware.php
@@ -10,3 +10,14 @@ use Slim\Csrf\Guard;
 
 // Add middleware for CSRF protection.
 $app->add($container[Guard::class]);
+
+// Redirect to HTTPS if necessary.
+$app->add(function ($request, $response, $next) {
+    if ($request->getUri()->getScheme() !== 'https') {
+        return $response->withRedirect(
+            $request->getUri()->withScheme('https')->withPort(null)
+        );
+    }
+
+    return $next($request, $response);
+});
